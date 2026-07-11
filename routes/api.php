b9e53968090a8f0a97c37399a7b44c3c0c843b99
VANSIEM190 <vansiemgracia03@gmail.com>
2026-07-11 20:43:03 +0100

    Work: Modifications locales sur les routes API

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/create-user', [App\Http\Controllers\UserController::class, 'store']);
-Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout'])->middleware('auth:sanctum');
-
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'getHomeData']);
-Route::get('/actualites', [\App\Http\Controllers\ActualiteController::class, 'index']);
-Route::get('/actualites/{id}', [\App\Http\Controllers\ActualiteController::class, 'show']);
 
