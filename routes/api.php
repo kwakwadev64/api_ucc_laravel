fb99e85a11524c6beb2b8c71d4b4f60e5ff442d4
VANSIEM190 <vansiemgracia03@gmail.com>
2026-07-11 21:34:56 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,66 @@ use App\Http\Controllers\Api\CourseController;
 require __DIR__.'/auth.php';
 
 
+
+
+/*
+|--------------------------------------------------------------------------
+| Options inscription
+|--------------------------------------------------------------------------
+*/
+
+Route::get(
+    '/register-options',
+    [RegisterOptionController::class, 'index']
+);
+
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Cours
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+
+    Route::get(
+        '/courses',
+        [CourseController::class, 'index']
+    );
+
+
+    Route::get(
+        '/courses/{course}',
+        [CourseController::class, 'show']
+    );
+
+
+    Route::post(
+        '/courses',
+        [CourseController::class, 'store']
+    );
+
+
+    Route::put(
+        '/courses/{course}',
+        [CourseController::class, 'update']
+    );
+
+
+    Route::delete(
+        '/courses/{course}',
+        [CourseController::class, 'destroy']
+    );
+
+
+    Route::get(
+        '/courses/{course}/download',
+        [CourseController::class, 'download']
+    );
+
+});
