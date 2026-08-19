2acc1723390e2124bbed65fdc475a234e9faebfd
VANSIEM190 <vansiemgracia03@gmail.com>
2026-08-19 02:39:47 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,19 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    
     'allowed_origins' => [
-        'https://fsiucc.com',
-        'https://www.fsiucc.com',
-        'http://localhost:5173',
-    ],
-    
-    'allowed_origins' => ['*'],
-    
-    'allowed_origins_patterns' => [],
-    
-    'allowed_headers' => ['*'],
-    
-    'exposed_headers' => [],
-    
-    'max_age' => 600, // Cache le preflight 10 min
-    
-    'supports_credentials' => false, // Mets true si tu utilises Sanctum/cookies
+    'https://fsiucc.com',
+    'https://www.fsiucc.com',
+    'http://localhost:5173',
+],
+
+'allowed_origins_patterns' => [],
+
+'allowed_headers' => ['*'],
+
+'exposed_headers' => [],
+
+'max_age' => 600,
+
+'supports_credentials' => false,
 ];
